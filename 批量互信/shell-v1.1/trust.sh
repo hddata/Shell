ebdabbc9fa71729e2f46b9c4659cca18d218f478
hddata <tiekaa@outlook.com>
2017-02-14 15:37:06 +0800

    up

--- 批量互信/shell-v1.1/trust.sh
+++ 批量互信/shell-v1.1/trust.sh
@@ -1,6 +1,14 @@
 #!/bin/bash   
 #trust.sh-v1.1
 #
+if [ "$#" -eq 1 -o "$#" -eq 2 ] ; then
+    a=1
+else
+    echo "First time trust   USAGE: sh trust.sh hostlist1"
+    echo "Second time trust  USAGE: sh trust.sh hostlist1 hostlist2"
+    exit -1
+fi
+
 checkfile()
 {
   if [ ! -f $1 ]
@@ -21,7 +29,7 @@ setkey()
     sshpass -p "$pass" ssh -t $user@$ip -o StrictHostKeyChecking=no "rm -rf ~/.ssh;\
     ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''" &> /dev/null
     if [ $? -eq 0 ] ; then
-        echo -e "成功建立公钥和私钥"
+        echo -e "Successfully established a public key and a private key"
     else
         echo "error: " $?
     fi
@@ -29,7 +37,7 @@ setkey()
     sshpass -p "$pass" scp -o StrictHostKeyChecking=no $user@$ip:~/.ssh/id_rsa.pub ~/id_rsa.pub1
     
     if [ $? -eq 0 ] ; then
-        echo -e "公钥成功传回本地\n"
+        echo -e "Back to the local public key success\n"
     else
         echo "error: " $?
     fi
@@ -41,7 +49,7 @@ setkey()
 var=`rpm -qa|grep sshpass`
 if [ ! -n "$var" ]; then
   echo "you should install sshpass first"
-  read -p "是否安装sshpass？是(y)---否(其他): "  tem
+  read -p "Install sshpass? yes(y)---else(exit): "  tem
   if [ "$tem" == 'y' -o "$tem" == 'Y' ]; then
     rpm -vih sshpass-1.05-7.1.x86_64.rpm
     rpm -ev sshpass-1.05-7.1.x86_64 --nodeps
@@ -59,7 +67,7 @@ sed -i '/^ *$/d' $hostlist_file1
 
 if [ "$#" -eq 1 ] ; then
     rm -rf authorized_keysrm
-    setkey $hostlist_file1 #互信hostlist_file1里面的主机
+    setkey $hostlist_file1
 elif [ "$#" -eq 2 ] ; then
     file_name2=$2
     hostlist_file2="$cwd/$file_name2"
@@ -67,13 +75,13 @@ elif [ "$#" -eq 2 ] ; then
     sed -i '/^ *$/d' $hostlist_file2 
     h2=`sed -n '$=' $hostlist_file2`
     if [ "$h2" -eq 0 ] ; then 
-  	    echo $hostlist_file2"错误"
+  	    echo $hostlist_file2"ERROR"
   	    exit 1
     else
   	    while read line
         do
           if cat $hostlist_file1 | grep "$line" >/dev/null ;then
-              echo $line"已经在互信的名单里了"
+              echo $line"Already in the hostlist1"
               sed -i "/$line/d" $hostlist_file2
           fi
        done < $hostlist_file2
@@ -82,7 +90,7 @@ elif [ "$#" -eq 2 ] ; then
    setkey $hostlist_file2 $cmd_file
    cat $hostlist_file2 >> $hostlist_file1
 else
-	echo "参数错误"
+	echo "Parameter error"
 	exit 1
 fi
 
@@ -95,7 +103,7 @@ for i in `seq $num`;do
     sshpass -p "$pass" scp authorized_keys  $user@$ip:~/.ssh/
 done
 if [ $? -eq 0 ] ; then
-   echo -e "公钥成功发送到个主机\n"
+   echo -e "Public key successfully sent to the hosts\n"
 else
   echo "error: " $?
 fi
@@ -105,7 +113,7 @@ for i in `seq $num`;do
   ip1=`awk ''NR==$num' {print $1}' $hostlist_file1`
   user1=`awk ''NR==$num' {print $2}' $hostlist_file1`
   pass1=`awk ''NR==$num' {print $3}' $hostlist_file1`
-  echo -e "------------测试------------\n通过" $user1@$ip1 "登陆其他主机"
+  echo -e "------------Test------------\nThrough" $user1@$ip1 "landing"
   for i in `seq $num`;do
     ip=`awk ''NR==$i' {print $1}' $hostlist_file1`
     user=`awk ''NR==$i' {print $2}' $hostlist_file1`
@@ -115,7 +123,7 @@ for i in `seq $num`;do
 done
 
 if [ $? -eq 0 ] ; then
-   echo -e "互信成功\n"
+   echo -e "Trust successful\n"
 else
   echo "error: " $?
 fi
